3099224f8a5ddb5b220790cc4fc0e1a458a4a422
adavuruku <aabdulraheemsherif@gmail.com>
2021-07-12 01:35:39 +0100

    fix this

--- src/components/orders/OrdersDetails.js
+++ src/components/orders/OrdersDetails.js
@@ -15,12 +15,20 @@ const OrdersDetails = ({selectedOrder}) =>{
                 <div  className='card mb-4 p-3 shadow-sm border-dark'>
                     <div className="card-header">{product.productInfo.productName}</div>
                     <div className='card-body text-dark' >
-                        <p className="card-text"><strong>Quantity : </strong> {product.quantity} ({product.productInfo.productMeasure})</p>
-                        <p className="card-text"><strong>Unit Price. : </strong>&#8358;  <NumberFormat value={product.unitPrice} displayType={'text'} thousandSeparator={true}/> </p>
-                        <p className="card-text"><strong>Sub Total. : </strong>&#8358;  <NumberFormat value={product.unitPrice * product.unitPrice} displayType={'text'} thousandSeparator={true}/> </p>
-                        <p className="card-text"><strong>Product Description. : </strong><br/> 
-                        {product.productInfo.productDescription}
-                        </p>
+                        <div className="row">
+                            <div className="col-8">
+                            <p className="card-text"><strong>Quantity : </strong> {product.quantity} ({product.productInfo.productMeasure})</p>
+                            <p className="card-text"><strong>Unit Price. : </strong>&#8358;  <NumberFormat value={product.unitPrice} displayType={'text'} thousandSeparator={true}/> </p>
+                            <p className="card-text"><strong>Sub Total. : </strong>&#8358;  <NumberFormat value={product.unitPrice * product.quantity} displayType={'text'} thousandSeparator={true}/> </p>
+                            <p className="card-text"><strong>Product Description. : </strong><br/> 
+                            {product.productInfo.productDescription}
+                            </p>
+                            </div>
+                            <div className="col-4">
+                            <img src={product.productInfo.productImage} className="rounded img-responsive"  style={{height:'200px', width:'200px'}} alt= {product.productInfo.productName} />
+                            </div>
+                        </div>
+                       
                     </div>
                 </div>
             </div>
